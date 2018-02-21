7c90c2cdd5a533aa2b1860badcb9aa8c444aabfd
mmicu <mmicu0@gmail.com>
2018-02-21 17:30:20 +0100

    Add two examples

---
+++ examples/c_example.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    int i = 0;
+    char c = 'a';
+
+    printf("Hello world\n");
+
+    return 0;
+}
+
---
+++ examples/py_example.py
@@ -0,0 +1,10 @@
+import sys
+
+if __name__ == '__main__':
+	print("Hello world")
+	
+	ll = [e * 2 for e in [1, 2, 3]]
+	print(ll)
+
+	sys.exit(0)
+
